c510392b7bbcab10154e923b1f45ab7e50961f06
Lavitra379 <73737247+Lavitra379@users.noreply.github.com>
2022-04-12 16:22:36 +0530

    Add files via upload

---
+++ bat.js
@@ -0,0 +1,49 @@
+status =""
+objects = "";
+
+function preload()
+{
+   img = loadImage("Plant.jpeg");
+}
+
+function setup() {
+    canvas = createCanvas(640, 420);
+    canvas.position(590, 400)
+    objectDetector = ml5.objectDetector('cocossd', modelLoaded);
+    document.getElementById("status_bat").innerHTML = "Status : Detecting Objects";
+    document.getElementById("objects_bat").innerHTML = "Plants Detected : Numbers"
+  }
+
+  function modelLoaded() {
+    console.log("Model Loaded!");
+    status = true;
+    objectDetector.detect(img, gotResult);
+  }
+
+  function gotResult(error, results) 
+  {
+    if (error) {
+      console.log(error);
+    }
+    console.log(results);
+    objects = results;
+  }
+
+  function draw() {
+    image(img, 0, 0, 640, 420);
+  
+       if(status != "")
+        {
+          for (i = 0; i < objects.length; i++) {
+            document.getElementById("status_bat").innerHTML = "Status : Object Detected";
+            
+            fill("#FF0000");
+            percent = floor(objects[i].confidence * 100);
+            text(objects[i].label + " " + percent + "%", objects[i].x, objects[i].y,objects[i].length);
+            noFill();
+            stroke("#FF0000");
+            rect(objects[i].x, objects[i].y, objects[i].width, objects[i].height);
+
+            document.getElementById("objects_bat").innerHTML = "Plants Detected : 1";
+             }     }
+  }
---
+++ gun.js
@@ -0,0 +1,50 @@
+status ="";
+objects = [];
+img = "";
+
+function preload()
+{
+   img = loadImage("Suitcase.jpg");
+}
+
+function setup() {
+    canvas = createCanvas(640, 420);
+    canvas.position(590, 400)
+    objectDetector = ml5.objectDetector('cocossd', modelLoaded);
+    document.getElementById("status_gun").innerHTML = "Status : Detecting Objects";
+    document.getElementById("objects_gun").innerHTML = "Suitcases Detected : Numbers";
+  }
+
+  function modelLoaded() {
+    console.log("Model Loaded!");
+    status = true;
+    objectDetector.detect(img, gotResult);
+  }
+
+  function gotResult(error, results) 
+  {
+    if (error) {
+      console.log(error);
+    }
+    console.log(results);
+    objects = results;
+  }
+
+  function draw() {
+    image(img, 0, 0, 640, 420);
+  
+       if(status != "")
+        {
+          for (i = 0; i < objects.length; i++) {
+            document.getElementById("status_gun").innerHTML = "Status : Object Detected";
+            
+            fill("#FF0000");
+            percent = floor(objects[i].confidence * 100);
+            text(objects[i].label + " " + percent + "%", objects[i].x, objects[i].y,objects[i].length);
+            noFill();
+            stroke("#FF0000");
+            rect(objects[i].x, objects[i].y, objects[i].width, objects[i].height);
+
+            document.getElementById("objects_gun").innerHTML = "Suitcases Detected : 1";
+             }     }
+  }
